73bbac52de1cd08d1d7400c3a01d1962b991e637
Kridsada Inthason <59548878+krizad20@users.noreply.github.com>
2023-01-20 16:46:59 +0700

    $db

--- api/index.php
+++ api/index.php
@@ -1,14 +1,6 @@
 <?php
-$client = new MongoDB\Driver\Manager("mongodb://birdnestdatabase:birdnestmongodb@BirdNestProject");
+// mongodb+srv://birdnestdatabase:birdnestmongodb@cluster-0/test?retryWrites=true&w=majority
+// mongodb://birdnestdatabase:birdnestmongodb@BirdNestProject
+$client = new MongoDB\Driver\Manager("mongodb+srv://birdnestdatabase:birdnestmongodb@BirdNestProject/test?retryWrites=true&w=majority");
 
-$filter = ['id' => 2];
-$options = [
-   'projection' => ['_id' => 0],
-];
-$query = new MongoDB\Driver\Query($filter, $options);
-$rows = $client->executeQuery('BirdNestDB.Customer_info', $query); // $mongo contains the connection object to MongoDB
-foreach($rows as $r){
-   print_($r);
-}
-header('content-type: application/json');
-echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
+$db = $client->test;
