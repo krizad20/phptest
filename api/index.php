a597537b0ed8d49dfc6c44572020723e5335b7ec
Kridsada Inthason <59548878+krizad20@users.noreply.github.com>
2023-01-20 15:28:10 +0700

    db

--- api/index.php
+++ api/index.php
@@ -1,4 +1,8 @@
 <?php
+$client = new MongoDB\Client(
+    'mongodb+srv://birdnestdatabase:birdnestmongodb@cluster-0/test?retryWrites=true&w=majority'
+);
 
+$db = $client->test;
 header('content-type: application/json');
 echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
