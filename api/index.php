8e679225cc74eebbe1eccb3716d9815aec07c884
Kridsada Inthason <59548878+krizad20@users.noreply.github.com>
2023-01-20 17:06:07 +0700

    a

--- api/index.php
+++ api/index.php
@@ -3,7 +3,15 @@
 // mongodb://birdnestdatabase:birdnestmongodb@BirdNestProject
 $client = new MongoDB\Driver\Manager("mongodb://birdnestdatabase:birdnestmongodb@BirdNestProject.com");
 
-var_dump($client->getServers());
+$filter = ['id' => '001'];
+$options = [
+   'projection' => ['_id' => 0],
+];
+$query = new MongoDB\Driver\Query($filter, $options);
+$rows = $client->executeQuery('BirdNestDB.my_collection', $query); // $mongo contains the connection object to MongoDB
+foreach($rows as $r){
+   print_($r);
+}
 
 
 
