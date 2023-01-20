2e3f2d5930c9203eae7032ef61489ab32e8d7c35
Kridsada Inthason <59548878+krizad20@users.noreply.github.com>
2023-01-20 16:07:44 +0700

    d

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
-$client = new MongoDB\Driver\Manager("mongodb://birdnestdatabase:birdnestmongodb@BirdNestProject/BirdNestDB?retryWrites=true&w=majority");
+$client = new MongoDB\Driver\Manager("mongodb://birdnestdatabase:birdnestmongodb@BirdNestProject/");
 var_dump($client);
 header('content-type: application/json');
 echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
