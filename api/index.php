f14eff965b2e1f6f1951ace8d88d64e2545538c2
Kridsada Inthason <59548878+krizad20@users.noreply.github.com>
2023-01-20 16:57:55 +0700

    client

--- api/index.php
+++ api/index.php
@@ -2,6 +2,8 @@
 // mongodb+srv://birdnestdatabase:birdnestmongodb@cluster-0/test?retryWrites=true&w=majority
 // mongodb://birdnestdatabase:birdnestmongodb@BirdNestProject
 $client = new MongoDB\Driver\Manager("mongodb://birdnestdatabase:birdnestmongodb@BirdNestProject.BirdNestDB.com");
-var_dump($client);
+
+var_dump($client->getServers());
+
 
 
